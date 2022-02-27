33a8802532c24d04a8c5cf1ab2e9d0ced9ad2226
Mqbtw90 <100515793+Mqbtw90@users.noreply.github.com>
2022-02-27 18:51:07 +0700

    Create a%3 == 0.py

---
+++ a%3 == 0.py
@@ -0,0 +1,7 @@
+a = int(input("vvedite chislo"))
+if a%3 == 0:
+    print("chislo delitsya na 3")
+elif a%3 != 0:
+    print("chislo ne podhodit trebovaniyam")
+if a > 0:
+    print("chislo bol`she nulya")
