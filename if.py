ccbbdc05cdc1416f1d83a76658285b6c4aa22c87
Mqbtw90 <100515793+Mqbtw90@users.noreply.github.com>
2022-02-27 18:20:54 +0700

    Create if.py

---
+++ if.py
@@ -0,0 +1,14 @@
+num = int(input("введи число"))
+if num == 0:
+    print("ti naghal 0, genius")
+elif num == 1:
+    print("malo vzyal")
+elif num == 2:
+    print("da voz`mi bol`she")
+elif num == 3:
+    print("seredina")
+elif num == 4:
+    print("ughe poluchshe")
+elif num == 5:
+    print("maksimalochka")
+          
