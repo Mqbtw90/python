2ee0e376575d19a1e3253d52e44c42dac4b8449c
Mqbtw90 <100515793+Mqbtw90@users.noreply.github.com>
2022-03-06 19:04:34 +0700

    Create third log.py

---
+++ third log.py
@@ -0,0 +1,9 @@
+a = "kolos"
+b = 2345
+c = input("vvedite login")
+d = int(input("vvedite parol`"))
+while(a != c) or (b != d):
+    print("neverniy login")
+    c = input("vvedite login")
+    d = int(input("vvedite parol`"))
+print("vse verno")
