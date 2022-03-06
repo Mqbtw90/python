f6d76fade94ac2891e37e3314dc9919d407f5a6e
Mqbtw90 <100515793+Mqbtw90@users.noreply.github.com>
2022-03-06 19:03:57 +0700

    Create second log.py

---
+++ second log.py
@@ -0,0 +1,9 @@
+a = "kolos"
+b = 2345
+c = input("vvedite login")
+d = int(input("vvedite parol`"))
+while(a != c) or (b != d):
+    print("ne verniy login ili porol`")
+    c = input("vvedite login")
+d = int(input("vvedite parol`"))
+print("vhod vipolnen")
