ef97fa03619e95033426c64ce69ad15d451c60e6
Mqbtw90 <100515793+Mqbtw90@users.noreply.github.com>
2022-03-06 19:03:23 +0700

    Create new file

---
+++ logandpar.py
@@ -0,0 +1,10 @@
+b = "kolos"
+c = 8967
+a = input("vvedite login")
+while(a != b):
+    a = input("login neverniy")
+print("login verniy")
+m = int(input("vvedite parol`"))
+while(c != m):
+    m = int(input("parol` neverniy"))
+print("parol` verniy")
