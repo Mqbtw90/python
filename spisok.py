9b2f49a6bb53f83d46dcb105560ea3e861d02cfe
Mqbtw90 <100515793+Mqbtw90@users.noreply.github.com>
2022-04-17 18:22:55 +0700

    Create spisok.py

---
+++ spisok.py
@@ -0,0 +1,8 @@
+products=["kartoshka", "kapusta", "morkov`"]
+print("chto mojno dobavit` eshe")
+a=input("vvedite ovosh` kotoriy hotite dobavit`")
+products.append(a)
+print("eshe cto-nibud`?")
+b=input("vvedite eshe odin ovosh")
+products.append(b)
+print(products)
